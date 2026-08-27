2609a8ea7cd846767af35f7d0afccfc5de0668cd
Kalyan Karn <karnkalyan@gmail.com>
2026-08-27 13:19:09 +0545

    fix(mxf): align MXF output parameters exactly with clean native broadcast standard

--- backend/recordingProfiles.js
+++ backend/recordingProfiles.js
@@ -276,8 +276,6 @@ const buildRecordingProfileArgs = (extension, filePath, options = {}) => {
 
     if (deinterlaceCompressed && profile.fallbackVideoFilter) {
         args.push('-vf', profile.fallbackVideoFilter);
-    } else if (profile.interlaced && !isCompressed) {
-        args.push('-vf', 'setfield=tff');
     }
 
     args.push('-c:v', videoEncoder);
